a4f0878a133b6bfa05c7760dd6e1bc873eb9bbb5
Juan Negrete <juan.negrete.dev@gmail.com>
2022-06-12 22:38:17 -0500

    Add color and clear buttons, fix canvas sizing, make it so that it only draws when pressing down on the mouse, not just by hovering

--- src/script.js
+++ src/script.js
@@ -1,18 +1,39 @@
 const sketchArea = document.querySelector(".sketchArea");
-let sketchPadSize = 13;
-sketchArea.style.gridTemplateColumns = `repeat(${sketchPadSize}, 50px)`;
+let sketchPadSize = 16;
+sketchArea.style.gridTemplateColumns = `repeat(${sketchPadSize}, 1fr)`;
+
+let mouseDown = 0;
+document.body.addEventListener('mousedown', (e) => {
+  ++mouseDown;
+});
+document.body.addEventListener('mouseup', (e) => {
+  --mouseDown;
+});
+
+const brushColor = document.querySelector('#brush-color');
+brushColor.value = '#000';
 
 for (let i = 0; i < sketchPadSize * sketchPadSize; i++) {
   let newDiv = document.createElement("div");
-  newDiv.classList.add("tile", `tile${i}`);
+  newDiv.classList.add("tile");
   sketchArea.appendChild(newDiv);
 }
 
 const sketchTiles = document.querySelectorAll(".tile");
-console.log(sketchTiles);
-sketchTiles.forEach((tile) => {
+sketchTiles.forEach(tile => {
   tile.addEventListener("mouseover", (e) => {
-    e.target.style.backgroundColor = "white";
+    if (mouseDown) e.target.style.backgroundColor = brushColor.value;
+  });
+});
+sketchTiles.forEach(tile => {
+  tile.addEventListener("mousedown", (e) => {
+    e.target.style.backgroundColor = brushColor.value;
   });
 });
 
+const clearButton = document.querySelector('#clear-button')
+clearButton.addEventListener('click', () => {
+  sketchTiles.forEach(tile => {
+    tile.style.backgroundColor = '#fff';
+  })
+})
