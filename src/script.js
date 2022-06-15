6f0dbb6d1ac615090bd238421da309c53679abfd
Juan Negrete <juan.negrete.dev@gmail.com>
2022-06-14 20:10:22 -0500

    Add rainbow brush functionality alongisde rainbow mode and classic mode buttons

--- src/script.js
+++ src/script.js
@@ -1,7 +1,25 @@
+// Return a random rgb value in a string.
+function randomRGB() {
+  const r = Math.floor((Math.random() * 255) + 1);
+  const g = Math.floor((Math.random() * 255) + 1);
+  const b = Math.floor((Math.random() * 255) + 1);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+function setGradient(target) {
+  
+}
+
+let brushMode = 'standard';
+
 const sketchArea = document.querySelector(".sketchArea");
 let sketchPadSize = 16;
 sketchArea.style.gridTemplateColumns = `repeat(${sketchPadSize}, 1fr)`;
 
+const brushColor = document.querySelector('#brush-color');
+brushColor.value = '#000'; //Reinitialize default color with each reload.
+
+// Detect if the mouse button is currently being pressed.
 let mouseDown = 0;
 document.body.addEventListener('mousedown', (e) => {
   ++mouseDown;
@@ -10,25 +28,44 @@ document.body.addEventListener('mouseup', (e) => {
   --mouseDown;
 });
 
-const brushColor = document.querySelector('#brush-color');
-brushColor.value = '#000';
 
 for (let i = 0; i < sketchPadSize * sketchPadSize; i++) {
   let newDiv = document.createElement("div");
-  newDiv.classList.add("tile");
+  newDiv.classList.add("tile"); // This sets the default background color as white in the css [rgb(255, 255, 255)].
   sketchArea.appendChild(newDiv);
 }
 
 const sketchTiles = document.querySelectorAll(".tile");
 sketchTiles.forEach(tile => {
   tile.addEventListener("mouseover", (e) => {
-    if (mouseDown) e.target.style.backgroundColor = brushColor.value;
+    if (mouseDown) {
+      switch (brushMode) {
+        case 'standard':
+          e.target.style.backgroundColor = brushColor.value;
+          break;
+        case 'rainbow':
+          e.target.style.backgroundColor = randomRGB();
+          break;
+        case 'gradient':
+
+          break;
+      }
+    } 
   });
 });
+
 sketchTiles.forEach(tile => {
   tile.addEventListener("mousedown", (e) => {
-    e.target.style.backgroundColor = brushColor.value;
-  });
+    switch (brushMode) {
+      case 'standard':
+        e.target.style.backgroundColor = brushColor.value;
+        break;
+      case 'rainbow':
+        e.target.style.backgroundColor = randomRGB();
+        break;
+      case 'gradient':
+        break;
+  }});
 });
 
 const clearButton = document.querySelector('#clear-button')
@@ -36,4 +73,21 @@ clearButton.addEventListener('click', () => {
   sketchTiles.forEach(tile => {
     tile.style.backgroundColor = '#fff';
   })
-})
+});
+
+const standardButton = document.querySelector('#standard-button');
+standardButton.addEventListener('click', () => {
+  brushMode = 'standard';
+});
+
+const rainbowButton = document.querySelector('#rainbow-button');
+rainbowButton.addEventListener('click', () => {
+  brushMode = 'rainbow';
+});
+
+const gradientButton = document.querySelector('#gradient-button');
+gradientButton.addEventListener('click', () => {
+  brushMode = 'gradient';
+});
+
+
